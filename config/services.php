a2ce843403feb1d954de2408cbd3b13dfb0c6e1d
fadyelgawly92 <fadyraafatelgawly@gmail.com>
2018-09-25 02:09:10 +0200

    register with slack done and fontawesome cdn and bower components installed and auth controller redirect to provider and handle call back methods are done and auth services file is done

---
+++ config/services.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => env('SES_REGION', 'us-east-1'),
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+    'slack' => [
+        'client_id' => '441385541137.441979989394',
+        'client_secret' => '7984f78fcce8139b7c3138f4fa3b9ef2',
+        'redirect' => 'http://slack.local/auth/slack/callback',
+    ],
+
+];
